6c8d0ebcb51a5176068decbc485eb6bba646dff3
Mike Tona <tonam01@mail.buffalostate.edu>
2021-07-30 15:52:39 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require_once './modules/header.php';
+
+echo getHeader();
+echo 'helloWorld';
---
+++ modules/header.php
@@ -0,0 +1,20 @@
+<?php function getHeader(){
+?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Weeb Calculator</title>
+  <meta name="description" content="A web app that calculates how much of a weeb you are">
+  <meta name="author" content="SitePoint">
+
+
+  <link rel="icon" href="/favicon.ico">
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+<?php
+}
